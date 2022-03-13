2b77e8392069b0830805b516cf2ccc54f6aae548
Fong Cheuk Ting <u3568878@connect.hku.hk>
2022-03-13 10:16:01 +0800

    commit

--- demo/settings.py
+++ demo/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from environs import Env
 
+env = Env()
+env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o8y)4%5hrlf@$1m2z521n*!^bg(t%-(z1)ts+(%x1tk02aw*+_'
+SECRET_KEY = env.str("QDD_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['safe-cove-32506.herokuapp.com', 'localhost', '127.0.0.1']
 
