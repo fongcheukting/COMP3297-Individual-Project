7a5423757c5ba348d7fc7743d50d87df744b58c6
Fong Cheuk Ting <u3568878@connect.hku.hk>
2022-03-13 10:19:19 +0800

    commit

--- demo/settings.py
+++ demo/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("QDD_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['safe-cove-32506.herokuapp.com', 'localhost', '127.0.0.1']
 
