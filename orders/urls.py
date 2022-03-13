00940a12646d416c50b72363f4c5b2567f70bbc9
Fong Cheuk Ting <u3568878@connect.hku.hk>
2022-03-13 10:06:21 +0800

    commit

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from orders import views
 
+
 urlpatterns = [
 path('hello', views.hello),
 path('view_all', views.view_all),
-path('QDD', views.QDD)
+path('QDD', views.QDD),
+path('', views.QDD, name='QDD')
 ]
