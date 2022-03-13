10c4a96c56747617795103c32d40a5dd1e3ca0f3
Fong Cheuk Ting <u3568878@connect.hku.hk>
2022-03-13 10:00:15 +0800

    commit

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import json
 import requests
 import ast
 import datetime
 # Create your views here.
+def hello(request):
+    return HttpResponse('Hello, user!')
+
+def view_all(request):
+    context = { "orders": [
+    { "time": "Feb. 1, 2022, 10:31 a.m.",
+    "status": "Delivered"
+    },
+    { "time": "Feb. 2, 2022, 2:25 p.m.",
+    "status": "Delivered"
+    },
+    { "time": "Feb. 2, 2022, 8:54 p.m.",
+    "status": "Confirmed"
+    },
+    { "time": "Jan. 3, 2020, 6:01 a.m.",
+    "status": "Confirmed"
+    }]}
+    return render(request, 'view_all.html', context=context)
+
 def QDD(request):
     connected = True
     #retrieve API2
