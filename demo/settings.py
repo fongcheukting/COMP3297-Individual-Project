a5b535af716a91f49a77dbc0fbb712634aca4a5b
Fong Cheuk Ting <u3568878@connect.hku.hk>
2022-03-13 08:51:29 +0800

    commit

--- demo/settings.py
+++ demo/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Set Allowed Hosts
+ALLOWED_HOSTS = ['safe-cove-32506.herokuapp.com/', 'localhost', '127.0.0.1']
